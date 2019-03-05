a837e1c18a9204939d9aebea02c03b1da3c05335
burgosLeo96 <burgosls@msn.com>
2019-03-05 09:57:42 -0500

    Static pages excercise update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'static_pages/home'
+
+  get 'static_pages/basics'
+
+  get 'static_pages/ruby'
+
+  get 'static_pages/git'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
